08c237f47f5d1a97de4ffc31dd0fdec5506865c1
Stefano Corra <stefano.corra@hotmail.com>
2022-04-26 19:36:12 -0400

    cartcontainer commit

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import { ChevronDown, ChevronUp } from '../icons'
 
-const CartItem = () => {
-  return <section>cartItem</section>
+const CartItem = ({ id, img, title, price, amount }) => {
+  return (
+    <article className='cart-item'>
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className='item-price'>${price}</h4>
+        <button className='remove-btn'>remove</button>
+      </div>
+      <div>
+        <button className='amount-btn'>
+          <ChevronUp />
+        </button>
+        <button className='amount-btn'>
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  )
 }
 
 export default CartItem
