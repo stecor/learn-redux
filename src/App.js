668be07fc7473f0938de514fb68dc6242ff31ae6
Stefano Corra <stefano.corra@hotmail.com>
2022-04-25 20:41:21 -0400

    cartcontainer commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar'
-
+import CartContainer from './components/CartContainer'
 function App() {
   return (
     <div className='App'>
       <main>
         <Navbar />
+        <CartContainer />
       </main>
     </div>
   )
