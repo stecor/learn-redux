10ddf84082b1f0823320078146a9106fd2e2193e
Stefano Corra <stefano.corra@hotmail.com>
2022-04-29 18:08:38 -0400

    cartcontainer commit

---
+++ src/components/Modal.js
@@ -0,0 +1,17 @@
+const Modal = () => {
+  return (
+    <aside className='modal-container'>
+      <div className='modal'>
+        <h4>Remove all items from your shopping cart?</h4>
+        <div className='btn-container'>
+          <button type='button' className='btn confirm-btn'>
+            confirm
+          </button>
+          <button className='btn clear-btn'>cancel</button>
+        </div>
+      </div>
+    </aside>
+  )
+}
+
+export default Modal
